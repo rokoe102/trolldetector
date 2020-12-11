81f3be9037bcd775bb630d945a3ccca465cdcd90
rokoe102 <rokoe102@uni-duesseldorf.de>
2020-12-11 20:31:44 +0100

    adapted performance metric terms to thesis

--- trolldetector/report/customreport.py
+++ trolldetector/report/customreport.py
@@ -1,11 +1,23 @@
 from sklearn import metrics
+from prettytable import PrettyTable, ALL
+
 
 # stores and prints the results of a custom classification
 class CustomReport:
     def __init__(self, y_test, predicted):
 
         self.conf = metrics.confusion_matrix(y_test, predicted)
-        self.clf_report = metrics.classification_report(y_test, predicted)
+
+        self.accuracy = metrics.accuracy_score(y_test, predicted)
+
+        self.precision = metrics.precision_score(y_test, predicted, pos_label="troll", zero_division=True)
+        self.npv = metrics.precision_score(y_test, predicted, pos_label="nontroll", zero_division=True)
+
+        self.recall = metrics.recall_score(y_test, predicted, pos_label="troll", zero_division=True)
+        self.specifity = metrics.recall_score(y_test, predicted, pos_label="nontroll", zero_division=True)
+
+        self.f1 = metrics.f1_score(y_test, predicted, pos_label="troll", zero_division=True)
+
 
     def print(self):
         print("+----------------------------------------------------+")
@@ -25,4 +37,13 @@ class CustomReport:
         print("+----------------------------------------------------+")
         print("|                performance metrics                 |")
         print("+----------------------------------------------------+")
-        print(self.clf_report)
+        score_table = PrettyTable(["accuracy", "precision", "npv", "recall", "specifity", "f1"])
+        score_table.add_row([round(self.accuracy,3),
+                             round(self.precision,3),
+                             round(self.npv,3),
+                             round(self.recall,3),
+                             round(self.specifity,3),
+                             round(self.f1,3)
+                            ])
+        print(score_table)
+
