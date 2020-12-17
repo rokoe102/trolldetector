5dc6d3f141e5a2a01c7d9f0d728fe97fa937437a
rokoe102 <rokoe102@uni-duesseldorf.de>
2020-12-17 02:46:40 +0100

    implemented input checks and warnings for all arguments

--- trolldetector/main.py
+++ trolldetector/main.py
@@ -6,109 +6,162 @@ from trolldetector.SVM import svm
 from trolldetector.all import all
 from trolldetector.dtree import dtree
 from trolldetector.parsing.commonarguments import CommonArguments
+from trolldetector.parsing import check
 
-#if __name__ == "__main__":
+
 def main():
-
     # define main parser
     pparser = argparse.ArgumentParser(prog="trolldetector")
+
     pparser._optionals.title = "Options"
-    pparser.add_argument("--optimize",dest="optimize", action="store_true", help="hyperparameter optimization (ignoring other options)")
+
+    pparser.add_argument("--optimize", dest="optimize", action="store_true",
+                         help="hyperparameter optimization (ignoring other options)")
 
     subparsers = pparser.add_subparsers(title="classification techniques", dest="command")
 
+
     # arguments all commands have in common
-    pparser.add_argument("--ngram",dest="gram", metavar="<n>",type=int,default=1, help="changing the n-gram range to (1,n)")
-    pparser.add_argument("--tfidf",dest="tfidf", action="store_true", help="changes the feature weighting to TF-IDF")
-    pparser.add_argument("--stopwords", dest="stop", action="store_true", help="enables filtering of english stop words")
-    pparser.add_argument("--dim", dest="dims",metavar="<components>", type=int,default=10, help="determines the level of dimensionality reduction")
-    pparser.add_argument("-v", "--verbose", dest="verb", action="store_true", help="producing more detailed output")
+    pparser.add_argument("--ngram", dest="gram", metavar="<n>", type=check.ngram_warning, default=1,
+                         help="changing the n-gram range to (1,n)")
+
+    pparser.add_argument("--tfidf", dest="tfidf", action="store_true",
+                         help="changes the feature weighting to TF-IDF")
+
+    pparser.add_argument("--stopwords", dest="stop", action="store_true",
+                         help="enables filtering of english stop words")
+
+    pparser.add_argument("--dim", dest="dims", metavar="<components>", type=check.dim_warning, default=10,
+                         help="determines the level of dimensionality reduction")
+
+    pparser.add_argument("--test", dest="test_set", metavar="<(0,1)>", type=check.test, default=0.1,
+                         help="changes the proportion of training and testing data")
+
 
     # parser for KNN command
-    knn_parser = subparsers.add_parser("KNN", add_help=False, help="k-nearest neighbor classification")
-    knn_parser.add_argument("-k", dest="kvar", metavar="<value>", type=int, default=5, help="changes k value")
-    knn_parser.add_argument("-m", dest="metric", metavar="<metric>", type=str, default="euclidean", choices=["euclidean", "manhattan", "chebyshev"], help="determines the metric for distance measurement")
-    knn_parser.add_argument("--test",dest="tpercKNN", metavar="<perc>", type=float,default=0.1, help="changes the proportion of test data")
-    knn_parser.add_argument("-h", dest="helpKNN", action="store_true", help="displays this help message")
+    knn_parser = subparsers.add_parser("KNN", add_help=False, help="k-Nearest Neighbor Classification")
+
+    knn_parser.add_argument("-k", dest="kvar", metavar="<n>", type=check.k_warning, default=5, help="changes k value")
+
+    knn_parser.add_argument("-m", dest="metric", type=str, default="euclidean", choices=["euclidean", "manhattan"],
+                            help="determines the metric for distance measurement")
+
+    knn_parser.add_argument("-h", dest="help_knn", action="store_true", help="displays this help message")
+
 
     # parser for NB command
-    nb_parser = subparsers.add_parser("NB", add_help=False, help="Naive Bayes classification")
-    nb_parser.add_argument("-d", "--distribution", dest="distNB", type=str, default="gaussian",choices = ["gaussian", "multinomial","bernoulli", "complement"], help="changes the presumed distribution of the data")
-    nb_parser.add_argument("--test", dest="tpercNB", metavar="<perc>", type=float, default=0.1, help="changes the proportion of test data")
-    nb_parser.add_argument("-h", dest="helpNB", action="store_true", help="displays this help message")
+    nb_parser = subparsers.add_parser("NB", add_help=False, help="Naive Bayes Classification")
+
+    nb_parser.add_argument("-d", "--dist", dest="distribution", type=str, default="gaussian",
+                           choices=["gaussian", "multinomial", "complement"],
+                           help="changes the presumed distribution of the data")
+
+    nb_parser.add_argument("-h", dest="help_nb", action="store_true", help="displays this help message")
+
 
     # parser for SVM command
-    svm_parser = subparsers.add_parser("SVM", add_help=False, help="support-vector machine classification")
-    svm_parser.add_argument("--cost", dest="cost", type=float, default=1.0, help="changes the penalization parameter for misclassification")
-    svm_parser.add_argument("--test", dest="tpercSVM", metavar="<perc>", type=float, default=0.1, help="changes the proportion of test data")
-    svm_parser.add_argument("-h", dest="helpSVM", action="store_true", help="displays this help message")
+    svm_parser = subparsers.add_parser("SVM", add_help=False, help="Support-vector Machine Classification")
+
+    svm_parser.add_argument("--cost", dest="cost", metavar="<(0,1]>", type=check.halfopen_interval, default=1.0,
+                            help="changes the penalization parameter for misclassification")
+
+    svm_parser.add_argument("-h", dest="help_svm", action="store_true", help="displays this help message")
+
 
     # parser for tree command
-    tree_parser = subparsers.add_parser("tree", add_help=False, help="decision tree classification")
-    tree_parser.add_argument("-m", dest="metrTree", type=str, default="gini", choices=["gini", "entropy"], help="determines the metric used for finding the best split")
-    tree_parser.add_argument("--test", dest="tpercTree", metavar="<perc>", type=float, default=0.1, help="changes the proportion of test data")
-    tree_parser.add_argument("-h", dest="helpTree", action="store_true", help="displays this help message")
+    tree_parser = subparsers.add_parser("tree", add_help=False, help="Decision Tree Classification")
+
+    tree_parser.add_argument("-m", dest="splitting", type=str, default="gini", choices=["gini", "entropy"],
+                             help="determines the metric used for finding the best split")
+
+    tree_parser.add_argument("-h", dest="help_tree", action="store_true", help="displays this help message")
+
 
     # parser for MLP command
-    mlp_parser = subparsers.add_parser("MLP", add_help=False, help="multi-layer perceptron classification")
-    mlp_parser.add_argument("-a", dest="actMLP", type=str, default="relu", choices=["relu","tanh","identity","logistic"], help="changes the activation function")
-    mlp_parser.add_argument("--iter", dest="iter", type=int, metavar="<n>", default=5, help="stops training after n iterations with no improvement >= tol")
-    mlp_parser.add_argument("--tol", dest="tol", type=float, default=0.0025, help="determines tolerance for stopping condition")
-    mlp_parser.add_argument("--test", dest="tpercMLP", metavar="<perc>", type=float, default=0.1, help="changes the proportion of test data")
-    mlp_parser.add_argument("-h", dest="helpMLP", action="store_true", help="displays this help message")
+    mlp_parser = subparsers.add_parser("MLP", add_help=False, help="Multilayer Perceptron Classification")
+
+    mlp_parser.add_argument("-a", dest="activation", type=str, default="relu", choices=["relu", "tanh", "logistic"],
+                            help="changes the activation function")
+
+    mlp_parser.add_argument("--iter", dest="iter", type=check.positive_integer, metavar="<n>", default=5,
+                            help="stops training after n iterations with no improvement >= tol")
+
+    mlp_parser.add_argument("--tol", dest="tol", type=check.tol_warning, metavar="<(0,1)>", default=0.0025,
+                            help="determines tolerance for stopping condition")
+
+    mlp_parser.add_argument("-h", dest="help_mlp", action="store_true", help="displays this help message")
+
 
     # parser for all command
     all_parser = subparsers.add_parser("all", add_help=False, help="compare all classificators")
+
+    all_parser.add_argument("-h", dest="help_all", action="store_true", help="displays this help message")
+
 
     # get arguments
     args = pparser.parse_args()
     method = args.command
 
     # wrap the shared arguments
-    cargs = CommonArguments(args.gram, args.tfidf, args.stop, args.dims, args.verb)
+    cargs = CommonArguments(args.gram, args.tfidf, args.stop, args.dims, args.test_set)
 
     # perform hyperparameter optimization if "--optimize" is provided
     # otherwise: execute technique once with custom hyperparameters
+    # (or print help message)
 
     if method == "KNN":
-        if args.helpKNN:
+        if args.help_knn:
             knn_parser.print_help()
+
         elif args.optimize:
-            knn.optimize(args.tpercKNN, args.verb)
+            knn.optimize()
+
         else:
-            knn.train_and_test(args.kvar, args.metric,  args.tpercKNN, cargs)
+            knn.train_and_test(args.kvar, args.metric, cargs)
 
     elif method == "NB":
-        if args.helpNB:
+        if args.help_nb:
             nb_parser.print_help()
+
         elif args.optimize:
-            nb.optimize(args.tpercNB, args.verb)
+            nb.optimize()
+
         else:
-            nb.train_and_test(args.tpercNB, args.distNB, cargs)
+            nb.train_and_test(args.distribution, cargs)
 
     elif method == "SVM":
-        if args.helpSVM:
+        if args.help_svm:
             svm_parser.print_help()
+
         elif args.optimize:
-            svm.optimize(args.tpercSVM, args.verb)
+            svm.optimize()
+
         else:
-            svm.train_and_test(args.tpercSVM,  args.cost,cargs)
+            svm.train_and_test(args.cost, cargs)
 
     elif method == "tree":
-        if args.helpTree:
+        if args.help_tree:
             tree_parser.print_help()
+
         elif args.optimize:
-            dtree.optimize(args.tpercTree, args.verb)
+            dtree.optimize()
+
         else:
-            dtree.train_and_test(args.tpercTree,  args.metrTree, cargs)
+            dtree.train_and_test(args.splitting, cargs)
 
     elif method == "MLP":
-        if args.helpMLP:
+        if args.help_mlp:
             mlp_parser.print_help()
+
         elif args.optimize:
-            mlp.optimize(args.tpercMLP, args.verb)
+            mlp.optimize()
+
         else:
-            mlp.train_and_test(args.actMLP,args.iter, args.tol,args.tpercMLP, cargs)
+            mlp.train_and_test(args.activation, args.iter, args.tol, cargs)
 
     elif method == "all":
-        all.compare(args.verb)
+        if args.help_all:
+            all_parser.print_help()
+
+        else:
+            all.compare()
---
+++ trolldetector/parsing/check.py
@@ -0,0 +1,84 @@
+import argparse
+
+
+# check if integer is greater than zero
+def positive_integer(value):
+    arg = int(value)
+    if arg <= 0:
+        raise argparse.ArgumentTypeError("%s is not an accepted input. must be positive" % arg)
+
+    return arg
+
+
+# check if input is in (0, 1)
+def open_interval(value):
+    arg = float(value)
+    if arg <= 0 or arg >= 1:
+        raise argparse.ArgumentTypeError("%s is not an accepted input. must be in interval (0, 1)" % arg)
+    return arg
+
+
+# check if input is in (0, 1]
+def halfopen_interval(value):
+    arg = float(value)
+    if arg <= 0 or arg > 1:
+        raise argparse.ArgumentTypeError("%s is not an accepted input. must be in interval (0, 1]" % arg)
+    return arg
+
+
+# check if input won't set test samples lower than 1
+def test(value):
+    arg = float(value)
+    if arg < 1.6e-06:
+        raise argparse.ArgumentTypeError("Value for test set share can not be lower than 1.6e-06")
+    return arg
+
+
+# warn if input will probably lead to high runtimes
+def dim_warning(value):
+    arg = positive_integer(value)
+    if arg >= 30:
+        print("Warning: high dimension value will probably lead to high runtime. proceed anyway? [y/n] ", end="")
+        check_choice()
+    return arg
+
+
+def k_warning(value):
+    arg = positive_integer(value)
+    if arg >= 75:
+        print("Warning: high k value will probably lead to high runtime. proceed anyway? [y/n] ", end="")
+        check_choice()
+
+    return arg
+
+
+def ngram_warning(value):
+    arg = positive_integer(value)
+    if arg >= 3:
+        print("Warning: ngram-range higher than (1,2) will probably lead to segmentation fault. proceed anyway? [y/n] ",
+              end="")
+        check_choice()
+    return arg
+
+
+def tol_warning(value):
+    arg = open_interval(value)
+    if arg <= 1e-05:
+        print("Warning: MLP tolerance lower than 1e-05 will probably lead to high runtime. proceed anyway? [y/n] ",
+              end="")
+        check_choice()
+    return arg
+
+
+def check_choice():
+    proceed = False
+    indifferent = 0
+    while not proceed:
+        answer = str(input())
+        if answer.lower() in ["y", "yes", "j", "ja"]:
+            proceed = True
+        elif answer.lower() in ["n", "no", "nein"] or indifferent >= 2:
+            exit(0)
+        else:
+            print("\n[y/n] ", end="")
+            indifferent += 1
